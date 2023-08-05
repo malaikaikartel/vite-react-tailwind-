9f0088241addcad38736756323503da70aa487c0
malaikaikartel <50528583+malaikaikartel@users.noreply.github.com>
2023-08-05 19:56:45 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,13 @@ import '@/lib/styles/globals.css';
 import App from './App';
 import MyLeaderBoardAd from './MyLeaderBoardAd';
 
-<MyLeaderBoardAd />
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+
     <App />
-  </React.StrictMode>
+<MyLeaderBoardAd /> 
+ </React.StrictMode>
 );
